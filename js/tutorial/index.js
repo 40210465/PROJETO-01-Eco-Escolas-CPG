7bf50e54e7433591913094744ed6521ec4f20e5b
TiagoRibeiro25 <tiago.d.ribeiro@hotmail.com>
2022-11-18 13:01:50 +0000

    added tutorial page

---
+++ js/tutorial/index.js
@@ -0,0 +1,21 @@
+const difficulties = document.querySelectorAll(".difficulty-card");
+const playAudio = new Audio("../../assets/sounds/play button.mp3");
+
+const values = {
+  Fácil: "easy",
+  Normal: "normal",
+  Impossível: "impossible",
+};
+
+difficulties.forEach((difficulty) => {
+  const option = values[difficulty.querySelector("h3").textContent];
+
+  difficulty.addEventListener("click", () => {
+    localStorage.difficulty = option;
+
+    // Wait a little so the audio may play
+    setTimeout(() => {
+      window.location.href = "../../html/game.html";
+    }, 200);
+  });
+});
